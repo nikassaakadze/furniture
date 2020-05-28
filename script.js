5f02133689fe8534eaa2ab8b5f8cf13d38ad0532
nikassaakadze <nikasaakadze@gmail.com>
2020-05-28 02:36:36 -0700

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+function responsiveDropDown(){
+//    const menuhide = document.querySelector('.header-nav-lists')
+    // const respimg = document.querySelector('.responsive-menu-icon')
+    // respimg.classList.add("responsive-icon-rotate")
+    // element.classList.add("mystyle")
+
+        const menuhide = document.querySelector('.header-nav-lists')
+        // const respimg = document.querySelector('.responsive-menu-icon')
+        // respimg.classList.toggle("responsive-icon-rotate");
+        menuhide.classList.toggle("responsive-menu-hidden");
+        
+}
